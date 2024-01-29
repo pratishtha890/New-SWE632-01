dc81dc0a1c2d47ab8bcf3bfe5b3c267c798081dc
pratishtha890 <pratishthapant890@gmail.com>
2024-01-28 22:09:45 -0500

    First

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/admin/login/login.component';
+import { LayoutComponent } from './pages/admin/layout/layout.component';
+import { FormsModule } from '@angular/forms';
+import { ProductsComponent } from './pages/admin/products/products.component';
+import { ContactComponent } from './pages/admin/contact/contact.component';
+import { AboutComponent } from './pages/admin/about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LayoutComponent,
+    ProductsComponent,
+    ContactComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
